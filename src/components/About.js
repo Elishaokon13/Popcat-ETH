7f22fd3ed26fc66b21ecc30e05ebde2bd65f9c47
Elishaokon13 <eliscodavid@gmail.com>
2024-07-29 07:54:02 +0100

    update

---
+++ src/components/About.js
@@ -0,0 +1,38 @@
+import Image from 'next/image';
+import React, { useState, useEffect } from 'react';
+import ImageRowWithSocials from './test';
+
+const AboutSection = () => {
+  const initialCount = 1624000;
+  const [counter, setCounter] = useState(initialCount);
+  const [power, setPower] = useState(1); // Start power from 1 to continue from initial count
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter(initialCount + (2 ** 4) * power);
+      setPower((prevPower) => prevPower + 1);
+    }, 1000); // Adjust the interval as needed
+
+    return () => clearInterval(interval);
+  }, [power]);
+
+  return (
+    <>
+      <div className="bg-cover bg-center bg-black bg-no-repeat " style={{ backgroundImage: "url('/bg.png')" }}>
+        <div className="relative min-h-screen w-full text-white p-8">
+          <div className="relative text-center">
+            <h1 className="text-3xl sm:text-5xl font-permanentMarker font-bold mb-8">PopCat; Another Cat that pops on ETH</h1>
+            <p className="max-w-4xl font-hanaleiFill mx-auto text-lg leading-relaxed">
+            Fuel your portfolio with the unstoppable meow-mentum of Popcat! This token is the purr-fect blend of humor and potential. Invest, sit back, and watch your assets pop like never before.
+
+              <br/>Together, we're unstoppable
+            </p>
+          </div>
+        </div>
+      </div>
+
+    </>
+  );
+};
+
+export default AboutSection;
---
+++ src/components/Hero.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Image from 'next/image';
+
+const HeroSection = () => {
+    const handleCopy = () => {
+        navigator.clipboard.writeText('TBD');
+        alert('Text copied to clipboard!');
+    };
+
+    return (
+        <>
+            <div className="bg-cover bg-center bg-black bg-no-repeat min-h-screen" style={{ backgroundImage: "url('/bg.png')" }}>
+                {/* Overlay with Text */}
+                <div className="relative z-10 flex flex-col items-center justify-center h-screen bg-black bg-opacity-50 p-4 md:p-8">
+                    <h1 className="text-[60px] sm:text-[120px] md:text-[100px] lg:text-[180px] text-shadow text-white font-permanentMarker font-extralight text-center">
+                        $POPCAT
+                    </h1>
+                    <Image
+                        src="/hero.PNG" // Replace with your image path
+                        alt="Centered Image"
+                        width={800} // Adjust width as needed
+                        height={600} // Adjust height as needed
+                        className="mt-8"
+                    />
+                    <div className="mt-4 p-2 flex flex-col md:flex-row items-center justify-center font-hanaleiFill  text-white rounded text-sm md:text-2xl font-bold w-full max-w-3xl">
+                        <span className="text-center md:text-left">CA: </span>
+                    </div>
+                    <div className="flex justify-center p-8">
+                        <button
+                            className="px-6 py-3 font-hanaleiFill text-white bg-black rounded font-bold hover:bg-gray-700 transition-colors duration-300"
+                        >
+                            Buy now
+                        </button>
+                    </div>
+                    <div className="flex justify-center bg-black space-x-4 md:space-x-8 pt-4">
+                        <a href="https://dexscreener.com/solana/" target="_blank" rel="noopener noreferrer">
+                            <Image src="/dexscreener.png" alt="Dexscreener" width={40} height={40} />
+                        </a>
+                        <a href="https://x.com/popcatoneth_" target="_blank" rel="noopener noreferrer">
+                            <Image src="/twitter.png" alt="Twitter" width={40} height={40} />
+                        </a>
+                        <a href="https://t.me/popcatoneth" target="_blank" rel="noopener noreferrer">
+                            <Image src="/tg.png" alt="Telegram" width={40} height={40} />
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default HeroSection;
